9122c843542d6cd984b5ffb7f6d7ab5f7405a1b0
Ravaughn Marsh <ravaughnmarsh@Ravs-MacBook-Air.local>
2026-02-25 02:05:58 -0500

    Add profile route, template, nav link and styles - Lab 2 Exercise 1

---
+++ app/views.py
@@ -0,0 +1,61 @@
+from app import app
+from flask import render_template, request, redirect, url_for, flash
+import datetime
+
+
+###
+# Routing for your application.
+###
+
+def format_date_joined(date):
+    """Return a date formatted as Month, Year (e.g. February, 2021)."""
+    return date.strftime("%B, %Y")
+
+
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html', name="Mary Jane")
+
+
+@app.route('/profile')
+def profile():
+    """Render the user's profile page."""
+    date_joined = datetime.date(2021, 2, 7)
+    formatted_date = format_date_joined(date_joined)
+    return render_template('profile.html', date_joined=formatted_date)
+
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also tell the browser not to cache the rendered page. If we wanted
+    to we could change max-age to 600 seconds which would be 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
